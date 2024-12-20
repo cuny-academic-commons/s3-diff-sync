482ed099558ff3da13db22f725c02924ce18d2ca
Boone B Gorges <boonebgorges@gmail.com>
2024-12-20 11:19:20 -0600

    Don't use async function for handle-acl-for-all-sites.php

--- handle-acl-for-all-sites.php
+++ handle-acl-for-all-sites.php
@@ -31,24 +31,37 @@ function log( $message, $run_log ) {
 
 $progress = \WP_CLI\Utils\make_progress_bar( 'Checking ACLs', count( $blog_ids ) );
 
+$instance = \S3_Uploads\Plugin::get_instance();
 foreach ( $blog_ids as $blog_id ) {
 	$progress->tick();
 
 	$blog_public = (int) get_blog_option( $blog_id, 'blog_public' );
+	$new_acl = $blog_public < 0 ? 'private' : 'public-read';
 
-	if ( $blog_public >= 0 ) {
-		// Nothing to do.
-		log( "$blog_id: blog_public is $blog_public. Skipping.", $run_log );
-		continue;
-	}
-
-	$instance = \S3_Uploads\Plugin::get_instance();
+	log( "Setting ACL for blog $blog_id to $new_acl", $run_log );
+	$counter = 0;
 
 	switch_to_blog( $blog_id );
 
-	cac_s3_uploads_update_acl_for_all_attachments( 'private' );
+	// Can't use cac_s3_uploads_update_acl_for_all_attachments() because it's async.
+	$attachments = get_posts(
+		array(
+			'post_type'      => 'attachment',
+			'posts_per_page' => -1,
+			'post_status'    => 'any',
+			'fields'         => 'ids',
+			'no_found_rows'  => true,
+		)
+	);
+
+	foreach ( $attachments as $attachment_id ) {
+		$instance->set_attachment_files_acl( $attachment_id, $new_acl );
+		++$counter;
+	}
 
 	restore_current_blog();
+
+	log( "Updated $counter attachments", $run_log );
 }
 
 $progress->finish();
